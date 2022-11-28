aa01749e58eee6e172eadb0834f2488f8db8df5f
liboxuan <lbxlilbx@cn.ibm.com>
2022-11-29 00:14:51 +0800

    create

---
+++ mock/login.ts
@@ -0,0 +1,49 @@
+// 根据角色动态生成路由
+import { MockMethod } from "vite-plugin-mock";
+
+export default [
+  {
+    url: "/login",
+    method: "post",
+    response: ({ body }) => {
+      debugger
+      if (body.username === "admin") {
+        return {
+          success: true,
+          data: {
+            username: "admin",
+            // 一个用户可能有多个角色
+            roles: ["admin"],
+            accessToken: "eyJhbGciOiJIUzUxMiJ9.admin",
+            refreshToken: "eyJhbGciOiJIUzUxMiJ9.adminRefresh",
+            expires: "2023/10/30 00:00:00"
+          }
+        };
+      } else if (body.username === "role1") {
+        return {
+          success: true,
+          data: {
+            username: "市场测试",
+            // 一个用户可能有多个角色
+            roles: ["role1"],
+            accessToken: "eyJhbGciOiJIUzUxMiJ9.role1",
+            refreshToken: "eyJhbGciOiJIUzUxMiJ9.role1Refresh",
+            expires: "2023/10/30 00:00:00"
+          }
+        };
+      } else {
+        return {
+          success: true,
+          data: {
+            username: "common",
+            // 一个用户可能有多个角色
+            roles: ["common"],
+            accessToken: "eyJhbGciOiJIUzUxMiJ9.common",
+            refreshToken: "eyJhbGciOiJIUzUxMiJ9.commonRefresh",
+            expires: "2023/10/30 00:00:00"
+          }
+        };
+      }
+    }
+  }
+] as MockMethod[];
